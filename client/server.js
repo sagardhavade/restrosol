725b1df9b0b64dc5d91b84aa3e4ed034ed4e03eb
sagardhavade <sagardhavade1998@gmail.com>
2025-02-13 17:07:48 +0530

    serverjs file add

--- client/server.js
+++ client/server.js
@@ -1,8 +1,26 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+// const { createServer } = require("http");
+// const { parse } = require("url");
+// const next = require("next");
 
-const port = process.env.PORT || 3000; // Use environment variable or default to 3000
+// const port = process.env.PORT || 3000; // Use environment variable or default to 3000
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`🚀 Server running at http://localhost:${port}`);
+//   });
+// });
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,8 +29,11 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`🚀 Server running at http://localhost:${port}`);
-  });
+  }).listen(port);
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`,
+  );
 });
